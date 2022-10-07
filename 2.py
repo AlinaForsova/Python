8295d711016b7b6f34016a6e1c3b196f9da03c03
AlinaForsova <k.forsova@gmail.com>
2022-10-07 13:52:22 +0300

    second commit

---
+++ 2.py
@@ -0,0 +1,7 @@
+First_cathetus=input('Введите значение первого катета')
+Second_cathetus=input('Введите значение второго катета')
+Area=(int(First_cathetus)*int(Second_cathetus))/2
+Hypotenuse=((int(First_cathetus))**2+(int(Second_cathetus))**2)**0.5
+Perimeter=(int(First_cathetus)+int(Second_cathetus)+int(Hypotenuse))
+print(Area,Perimeter)
+
